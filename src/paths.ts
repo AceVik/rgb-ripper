650ca8cd3432a43c5d02f96894120759124d6aaf
AceVik <mail@acevik.de>
2025-02-18 16:24:37 +0100

    Quickfix

--- src/paths.ts
+++ src/paths.ts
@@ -3,7 +3,7 @@ import * as process from 'node:process';
 import * as fs from 'node:fs';
 
 export const rootDir = path.resolve(__dirname, '..');
-export const filesDir = path.resolve(rootDir, process.env.FILES_DIR || 'files');
+export const filesDir = process.env.FILES_DIR || path.resolve(rootDir, 'files');
 export const imagesDir = path.resolve(filesDir, 'images');
 export const videosDir = path.resolve(filesDir, 'videos');
 export const dbPath = path.resolve(filesDir, 'db.sqlite');
